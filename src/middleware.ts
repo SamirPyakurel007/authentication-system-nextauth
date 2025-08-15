198424230e16c9809fbf0f902fa8e21ffd6ad0cb
samir <samirpyakurel07@gmail.com>
2025-08-15 05:48:17 +0545

    tested out where to put layout

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,8 +11,7 @@ export async function middleware(request: NextRequest) {
     token &&
     (url.pathname.startsWith("/signIn") ||
       url.pathname.startsWith("/signUp") ||
-      url.pathname.startsWith("/verify") ||
-      url.pathname === "/")
+      url.pathname.startsWith("/verify"))
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
